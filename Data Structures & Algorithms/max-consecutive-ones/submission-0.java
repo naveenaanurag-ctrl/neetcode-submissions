dd552671860cce20e4fdfe430859d26ec45dd056
naveenaanurag-ctrl <naveenaanurag@gmail.com>
2026-05-05 11:26:33 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int currentStreak=0;
+        int maxStreak=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                currentStreak++;
+                maxStreak=Math.max(maxStreak,currentStreak);
+            }
+            else
+            {
+                currentStreak=0;
+            }
+
+        }
+        return maxStreak;
+    }
+}
